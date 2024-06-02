57157fa2644d7deda612fd3ffe6f5db060818574
Plasius <Plasius@users.noreply.github.com>
2024-06-02 18:15:41 +0300

    implemented kubernetes

--- customer/src/main/java/eu/miraiworks/customer/CustomerApplication.java
+++ customer/src/main/java/eu/miraiworks/customer/CustomerApplication.java
@@ -3,6 +3,8 @@ package eu.miraiworks.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,6 +13,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         }
 )
 @EnableFeignClients(basePackages = "eu.miraiworks.clients")
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- notification/src/main/java/eu/miraiworks/notification/NotificationApplication.java
+++ notification/src/main/java/eu/miraiworks/notification/NotificationApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
                 "eu.miraiworks.notification",
                 "eu.miraiworks.amqp",
         }
+)
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 )
 public class NotificationApplication {
     public static void main(String[] args) {
